c243ce489fccf4e4971af6407de03616a76d3991
Lawson Caudill <lawson@getthinktank.com>
2020-07-01 17:14:49 +0100

    Updated packages and added basic bootstrappers.

--- Naos.MachineManagement.Test/SampleTest.cs
+++ Naos.MachineManagement.Test/SampleTest.cs
@@ -6,10 +6,23 @@
 
 namespace Naos.MachineManagement.Test
 {
+    using OBeautifulCode.Assertion.Recipes;
+    using Xunit;
+
     /// <summary>
     /// Test class.
     /// </summary>
-    public class SampleTest
+    public static class SampleTest
     {
+        /// <summary>
+        /// This is an example of how to use this logic to connect to another computer and run a script.
+        /// </summary>
+        [Fact(Skip = "Example to connect to another machine.")]
+        public static void ExampleToConnectToAnotherMachine()
+        {
+            var machineManager = new Naos.MachineManagement.WinRm.WinRmMachineManager("10.0.0.1", "Administrator", "Password", true);
+            var output = machineManager.RunScript("ls c:/");
+            output.MustForTest().NotBeNull();
+        }
     }
 }
